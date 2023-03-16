2eccc8394f02acebc4f6f5516963de6a880a08f9
1C0D <63018905+1C0D@users.noreply.github.com>
2023-03-16 22:21:24 +0100

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,21 @@
 import { Plugin } from "obsidian";
 
-export default class MyPlugin extends Plugin {
+export default class revealExplorerFile extends Plugin {
 	async onload() {
 		this.app.workspace.onLayoutReady(() => {
+			this.reveal();
 			this.registerEvent(
 				this.app.workspace.on("active-leaf-change", () => {
-					const containerEl = this.app.workspace.containerEl.win; //win to work on multi windows 
-					this.registerDomEvent(
-						containerEl,
-						"click",
-						this.clickHandler
-					);
+					this.reveal();
 				})
 			);
 		});
 	}
+
+	reveal = () => {
+		const containerEl = this.app.workspace.containerEl.win; //win to work on multi windows
+		this.registerDomEvent(containerEl, "click", this.clickHandler);
+	};
 
 	clickHandler = async (evt: any) => {
 		if (evt.target.classList.contains("view-header-title")) {
